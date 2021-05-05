c9b3024b7e5c982b44434f2186d49c134ce34352
Nérimène <nerimene.brahim@gmail.com>
2021-05-05 16:45:13 +0100

    Add files via upload

---
+++ GithubLoader/GithubLoader/GithubLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubLoaderApp.swift
+//  GithubLoader
+//
+//  Created by Nerimene on 4/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct GithubLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
